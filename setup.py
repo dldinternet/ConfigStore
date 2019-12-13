4a6ee288b6a8d0c70f85540355fb81505d8242c1
Christo De Lange <christo.delange@tekstream.com>
2019-12-12 21:28:16 -0500

    Retry 1.0.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = read('README.md')
 
 setup(
     name='pyconfigstore',
-    version='1.0.7',
+    version='1.0.5',
     description=(
         'A Python module for handling config files. It helps handles persist config files and also giving the ability to set, get, update and delete config settings'
           ' its based on Configstore.js'
